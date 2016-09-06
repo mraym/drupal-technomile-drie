2e8a4107723198ea39683db13e006a81a89a6eb9
Martin R <sizlaksystems@gmail.com>
2016-09-06 13:30:00 +0200

    Tinkering with drupal install timeout again

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -581,4 +581,6 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
-ini_set('max_execution_time', 0);
+
+# setting bigger timeout for install
+ini_set('max_execution_time', 300);
